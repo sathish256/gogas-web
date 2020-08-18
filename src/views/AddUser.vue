6b3542bbd4d74bad2039875e238744310e43d75f
Nasruddin Asgar <nasruddin.a@enchanting-travels.com>
2020-08-18 19:10:52 +0530

    Adds user data model

--- src/views/AddUser.vue
+++ src/views/AddUser.vue
@@ -4,120 +4,160 @@
       <h1>Add User</h1>
       <b-button class="my-2" variant="success">Create</b-button>
     </div>
-    <div class="from-layout pr-4">
+    <div class="form-layout pr-4">
       <b-card title="User Info">
+        <b-row>
+          <b-col cols="12" md="6">
+            <b-row>
+              <b-col cols="12" md="6">
+                <b-form-group label="First Name" label-for="first-name">
+                  <b-form-input
+                    id="first-name"
+                    v-model="userInfo.firstName"
+                    required
+                    placeholder="Enter first name"
+                  />
+                </b-form-group>
+              </b-col>
+              <b-col cols="12" md="6">
+                <b-form-group label="Last Name" label-for="last-name">
+                  <b-form-input
+                    id="last-name"
+                    v-model="userInfo.lastName"
+                    required
+                    placeholder="Enter last name"
+                  />
+                </b-form-group>
+              </b-col>
+              <b-col cols="12" md="6">
+                <b-form-group label="Phone" label-for="user-phone">
+                  <b-form-input
+                    id="user-phone"
+                    v-model="userInfo.phone"
+                    required
+                    placeholder="Enter phone"
+                  />
+                </b-form-group>
+              </b-col>
+              <b-col cols="12" md="6">
+                <b-form-group label="Role" label-for="role">
+                  <b-form-select
+                    required
+                    v-model="userInfo.role"
+                    :options="userRoles"
+                  >
+                    <template v-slot:first>
+                      <b-form-select-option :value="null" disabled>
+                        Select Role
+                      </b-form-select-option>
+                    </template>
+                  </b-form-select>
+                </b-form-group>
+              </b-col>
+            </b-row>
+          </b-col>
+          <b-col
+            cols="12"
+            md="1"
+            offset-md="5"
+            class="d-flex flex-column justify-content-center"
+          >
+            <b-img
+              thumbnail
+              src="https://picsum.photos/250/250/?image=54"
+              alt="Avatar"
+            />
+            <b-button variant="primary">Upload</b-button>
+          </b-col>
+        </b-row>
+      </b-card>
+      <b-card class="mt-3" title="Address">
+        <b-row>
+          <b-col cols="12" md="3">
+            <b-form-group label="House No" label-for="House-no">
+              <b-form-input
+                id="house-no"
+                v-model="address.houseNo"
+                required
+                placeholder="Enter house #"
+              />
+            </b-form-group>
+          </b-col>
+          <b-col cols="12" md="3">
+            <b-form-group label="Street" label-for="street">
+              <b-form-input
+                id="street"
+                v-model="address.street"
+                required
+                placeholder="Enter street"
+              />
+            </b-form-group>
+          </b-col>
+          <b-col cols="12" md="3">
+            <b-form-group label="Locality" label-for="locality">
+              <b-form-input
+                id="locality"
+                v-model="address.locality"
+                required
+                placeholder="Enter locality"
+              />
+            </b-form-group>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="12" md="3">
+            <b-form-group label="City" label-for="city">
+              <b-form-input
+                id="city"
+                v-model="address.city"
+                required
+                placeholder="Enter city"
+              />
+            </b-form-group>
+          </b-col>
+          <b-col cols="12" md="3">
+            <b-form-group label="State" label-for="state">
+              <b-form-input
+                id="state"
+                v-model="address.state"
+                required
+                placeholder="Enter state"
+              />
+            </b-form-group>
+          </b-col>
+          <b-col cols="12" md="3">
+            <b-form-group label="PIN" label-for="pin">
+              <b-form-input
+                id="pin"
+                v-model="address.pincode"
+                required
+                placeholder="Enter pincode"
+              />
+            </b-form-group>
+          </b-col>
+        </b-row>
+      </b-card>
+      <b-card class="mt-3 mb-5" title="Emergency Contact">
         <b-form>
           <b-row>
-            <b-col cols="12" md="6" lg="3">
-              <b-form-group label="First Name" label-for="first-name">
+            <b-col cols="12" md="3">
+              <b-form-group label="Name" label-for="emergency-name">
                 <b-form-input
-                  id="first-name"
+                  id="emergency-name"
+                  v-model="emergencyContact.name"
                   required
                   placeholder="Enter name"
                 />
               </b-form-group>
             </b-col>
-            <b-col cols="12" md="6" lg="3">
-              <b-form-group label="Last Name" label-for="last-name">
+            <b-col cols="12" md="3">
+              <b-form-group label="Phone" label-for="emergency-phone">
                 <b-form-input
-                  id="last-name"
+                  id="emergency-phone"
+                  v-model="emergencyContact.phone"
                   required
-                  placeholder="Enter Last Name"
+                  placeholder="Enter phone"
                 />
-              </b-form-group>
-            </b-col>
-
-            <b-col cols="12" md="3" lg="5">
-              <div>
-                <b-avatar src size="6rem"></b-avatar>
-              </div>
-            </b-col>
-          </b-row>
-          <div>
-            <b-button variant="success">Upload Image</b-button>
-          </div>
-
-          <b-row>
-            <b-col cols="12" md="6" lg="3">
-              <b-form-group label="Phone" label-for="phone">
-                <b-form-input
-                  id="phone"
-                  required
-                  placeholder="Enter phone number"
-                />
-              </b-form-group>
-            </b-col>
-
-            <b-col cols="12" md="6" lg="3">
-              <b-form-group label="Role" label-for="role">
-                <b-form-select
-                  required
-                  placeholder="selectRole"
-                  v-model="role"
-                  :options="options"
-                ></b-form-select>
-              </b-form-group>
-            </b-col>
-          </b-row>
-        </b-form>
-      </b-card>
-      <b-card class="mt-3" title="Address">
-        <b-form>
-          <b-row>
-            <b-col cols="12" md="6" lg="3">
-              <b-form-group label="House No" label-for="House-no">
-                <b-form-input
-                  id="house-no"
-                  required
-                  placeholder="Enter House no"
-                />
-              </b-form-group>
-            </b-col>
-            <b-col cols="12" md="6" lg="3">
-              <b-form-group label="Street" label-for="street">
-                <b-form-input id="street" required placeholder="Enter Street" />
-              </b-form-group>
-            </b-col>
-            <b-col cols="12" md="6" lg="3">
-              <b-form-group label="Locality" label-for="locality">
-                <b-form-input
-                  id="locality"
-                  required
-                  placeholder="Enter Locality"
-                />
-              </b-form-group>
-            </b-col>
-          </b-row>
-          <b-row>
-            <b-col cols="12" md="6" lg="3">
-              <b-form-group label="City" label-for="city">
-                <b-form-input id="city" required placeholder="Enter City" />
-              </b-form-group>
-            </b-col>
-            <b-col cols="12" md="6" lg="3">
-              <b-form-group label="State" label-for="state">
-                <b-form-input id="state" required placeholder="Enter State" />
-              </b-form-group>
-            </b-col>
-            <b-col cols="12" md="6" lg="3">
-              <b-form-group label="PIN" label-for="pin">
-                <b-form-input id="pin" required placeholder="Enter Pin" />
-              </b-form-group>
-            </b-col>
-          </b-row>
-        </b-form>
-      </b-card>
-      <b-card class="mt-3 mb-5" title="Emergency Contact">
-        <b-form>
-          <b-row>
-            <b-col cols="12" md="6" lg="3">
-              <b-form-group label="Name" label-for="name">
-                <b-form-input id="name" required placeholder="Enter Name" />
-              </b-form-group>
-            </b-col>
-            <b-col cols="12" md="6" lg="3">
-              <b-form-group label="Phone" label-for="phone">
-                <b-form-input id="phone" required placeholder="Enter Phone" />
               </b-form-group>
             </b-col>
           </b-row>
@@ -133,13 +173,20 @@ export default {
 
   data() {
     return {
-      role: null,
-      options: [
-        { value: "a", text: "C & F" },
-        { value: "b", text: "Dealer" },
-        { value: "c", text: "Marketing" },
-        { value: "d", text: "Delivery" }
-      ]
+      userInfo: { firstName: "", lastName: "", phone: "", role: null },
+      userRoles: ["C & F", "Dealer", "Marketing", "Delivery"],
+      address: {
+        houseNo: "",
+        street: "",
+        locality: "",
+        city: "",
+        state: "",
+        pincode: ""
+      },
+      emergencyContact: {
+        name: "",
+        phone: ""
+      }
     };
   }
 };
